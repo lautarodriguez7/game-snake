52490ec53f33cf30650ea63832737fbfe11395df
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-07-31 10:10:10 -0300

    adding code

--- js/game.js
+++ js/game.js
@@ -108,6 +108,34 @@ Rectangle.prototype = {
     }
 };
 
+/*Rectangle.prototype.intersects = function (rect) {
+    if (rect === undefined) {
+        window.console.warn('Missing parameters on function intersects');
+    } else {
+        return (this.x < rect.x + rect.width &&
+            this.x + this.width > rect.x &&
+            this.y < rect.y + rect.height &&
+            this.y + this.height > rect.y);
+        }
+    };
+Rectangle.prototype.fill = function (ctx) {
+    if (ctx === undefined) {
+        window.console.warn('Missing parameters on function fill');
+    } else {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
+Rectangle.prototype.drawImage = function (ctx, img) {
+    if (img === undefined) {
+        window.console.warn('Missing parameters on function drawImage');
+    } else {
+        if (img.width) {
+            ctx.drawImage(img, this.x, this.y);
+        } else {
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
+    }
+};*/
 
 function random(max) {
     //return Math.floor(Math.random() * max);
